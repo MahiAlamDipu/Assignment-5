6dc08561ba34355fd6688907e94b784d65b96af8
mahi-2003099 <mahialamdipu99@gamil.com>
2025-03-05 13:08:19 +0600

    after adding all the part

---
+++ scripts/discover.js
@@ -0,0 +1,4 @@
+document.addEventListener('click', function (event) {
+    if (event.target.id == 'back-to-desk')
+        window.location.href = 'index.html'
+})
--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,21 @@ const colorPicker = getElementById('colorPicker');
 const body = getElementById('body');
 
 const taskAssigned = getElementById('task-assigned')
-const allTask = document.getElementsByClassName('task')
-taskAssigned.innerText=String(allTask.length).padStart(2, '0')
+let allTask = document.getElementsByClassName('task')
+taskAssigned.innerText = String(allTask.length).padStart(2, '0')
 
 const taskCompleted = getElementById('task-completed')
 
 const dayName = getElementById('day-name')
 
 const day = now.getDay()
-const dayNames = ['Sun' , 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 dayName.innerText = dayNames[day]
 
 const date = getElementById('date')
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-date.innerText = months[now.getMonth()]+' ' + now.getDate() +' ' + now.getFullYear()
+date.innerText = months[now.getMonth()] + ' ' + now.getDate() + ' ' + now.getFullYear()
 console.log(now.getDate())
 
 
@@ -39,39 +39,47 @@ clear_history.addEventListener('click', function () {
 
 
 const completed = getElementById('completed')
-completed.addEventListener('click', function (event) {
+document.addEventListener('click', function (event) {
+
     event.preventDefault();
 
-    // add the event in the history
-    const task = getElementById('task').innerText;
+    if (event.target.id == 'completed') {
 
-    console.log(task)
-    
-    let hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
+        alert("Board Updated Successfully")
 
-    const complete_time = hours + ':' + minutes + ':' + seconds + ' ' + ampm
-    console.log(complete_time)
-    
-    
-    const new_history = document.createElement('div');
-    new_history.className = "bg-[#F4F7FF] p-2 rounded-2xl";
-    new_history.innerHTML = `<p>You have completed the task <span class="font-medium">${task}</span> at <span>${complete_time}</span></p>`;
-
-    // Append it to the activity_history element
-    activity_history.appendChild(new_history);
+        // add the event in the history
+        const task = event.target.parentNode.parentNode.children[1].innerText
 
 
-    // disable the button
-    completed.style.backgroundColor = 'lightgrey'
-    taskAssigned.innerText=String(--(allTask.length)).padStart(2, '0')
-    taskCompleted.innerText=String(++(taskCompleted.innerText)).padStart(2, '0')
-    completed.disabled = true;
+        let hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
 
+        const complete_time = hours + ':' + minutes + ':' + seconds + ' ' + ampm
+
+        const new_history = document.createElement('div');
+        new_history.className = "bg-[#F4F7FF] p-3 rounded-2xl";
+        new_history.innerHTML = `<p>You have completed the task <span class="font-medium">${task}</span> at <span>${complete_time}</span></p>`;
+
+
+        activity_history.appendChild(new_history);
+
+
+        // disable the button
+        event.target.style.backgroundColor = 'lightgrey'
+        if (taskAssigned.innerText == '01')
+            alert("Congrates!! You have completed all the current task")
+
+        taskAssigned.innerText = String(--(taskAssigned.innerText)).padStart(2, '0')
+        taskCompleted.innerText = String(++(taskCompleted.innerText)).padStart(2, '0')
+        event.target.disabled = true;
+    }
+    if(event.target.id=='discover')
+        window.location.href ='discover.html'
 
 })
+
 
 
 
